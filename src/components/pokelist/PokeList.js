657fc8d772994dacf7a7eff9bde08f2920cb34ae
Stephen <steve.wiggins7@gmail.com>
2021-03-08 00:50:54 -0500

    Add the pokemon api wrapper

--- src/components/pokelist/PokeList.js
+++ src/components/pokelist/PokeList.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
+const Pokedex = require("pokeapi-js-wrapper")
+const P = new Pokedex.Pokedex()
 
 function PokeList() {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);
-
+  // or with Promises
+  let pikachu = P.getPokemonByName("pikachu")
+  .then(function(response) {
+    console.log(response)
+  })
   return (
     <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
+      
     </div>
   );
 }
