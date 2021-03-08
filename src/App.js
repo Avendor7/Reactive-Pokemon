8f983b69eecd1f8168f2d2cabc32da3a7d0ecc24
stephen <steve.wiggins7@gmail.com>
2021-03-07 21:27:14 -0500

    create the components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import PokeDetails from './components/pokedetails/PokeDetails';
+import PokeList from './components/pokelist/PokeList';
 
 function App() {
   return (
     <div className="App">
-      
+      <PokeDetails />
+      <PokeList />
     </div>
   );
 }
