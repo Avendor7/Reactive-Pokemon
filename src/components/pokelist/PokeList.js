6c479f4e0ec9dea6718b847683e58946fcd413d2
stephen <steve.wiggins7@gmail.com>
2021-03-12 20:14:41 -0500

    delete some files

--- src/components/pokelist/PokeList.js
+++ src/components/pokelist/PokeList.js
@@ -6,10 +6,7 @@ function PokeList() {
 
   return (
     <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
+     
     </div>
   );
 }
