9f1359c0043215fd05fa01a9ce34f67f8a254de3
stephen <steve.wiggins7@gmail.com>
2021-03-12 23:39:24 -0500

    boilerplate code for api and searchbox

---
+++ src/components/searchbox/Searchbox.js
@@ -0,0 +1,13 @@
+import React, {  } from 'react';
+
+function Searchbox() {
+  // Declare a new state variable, which we'll call "count"
+ 
+  return (
+    <div className="searchbox">
+        <label htmlFor="searchbox"></label>
+        <input id="searchbox"></input>
+    </div>
+  );
+}
+export default Searchbox;
